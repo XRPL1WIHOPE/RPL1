1e9aee03d5d64d3f44bde1f0b8bda695b2daed28
XRPL1WIHOPE <xrpl10wiHope@gmail.com>
2023-07-30 08:18:23 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+// global variable start
+const sekolah = 'https://instagram.com/official.smkwiraharapan?igshid=MzRlODBiNWFlZA==';
+const grup = 'https://instagram.com/expors.ofc?igshid=MzRlODBiNWFlZA==';
+// global variable end
+
 // navbar script start
 let nilai = 0;
 const button = document.getElementById("button-nav");
@@ -6,18 +11,21 @@ button.addEventListener("click", () => {
   nilai = (nilai + 1) % 2;
   button.setAttribute("src", `./assets/components/${nilai === 0 ? "menu" : "x"}.svg`)
   nav_menu.classList.toggle("interak");
-})
-// navbar script end
-
+});
+// navbar script endb 
 
 // home start
 document.getElementById("visBtn").addEventListener("click", () => {
-  window.open("https://instagram.com/expors.ofc?igshid=MzRlODBiNWFlZA==");
+  window.open(`${grup}`);
 });
 // home end
 
 // about start
 document.getElementById("sekolah").addEventListener("click", () => {
-  window.open("https://instagram.com/official.smkwiraharapan?igshid=MzRlODBiNWFlZA==");
+  window.open(`${sekolah}`);
+})
+
+document.getElementById("groupN").addEventListener("click", () => {
+  window.open(`${grup}`)
 })
 // about end
