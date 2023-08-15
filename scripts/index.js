4ddab17c5a55ddc19666d1a1a0dfe679eb0ec0e7
WidianaPutra <olenggamer@gmail.com>
2023-08-15 09:13:28 +0800

    update

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ button.addEventListener("click", () => {
 });
 
 document.getElementById("game").addEventListener("click", function () {
-  window.open("./game/game.html");
+  window.location.href = "./game/game.html";
 });
 // navbar script end
 
