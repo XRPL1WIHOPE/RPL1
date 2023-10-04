169a6ce5fb132c3f746e0720d65412bba2fe5ade
WidianaPutra <olenggamer@gmail.com>
2023-10-04 19:42:47 +0800

    remake

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,71 +1,87 @@
-$(".information").css({
-  transform: "translate(0%)",
-  transition: "1s",
+const scrollValue = $(this).scrollTop();
+const tinggiDokumen = $(document).height();
+const tinggiJendela = $(window).height();
+
+const persScroll = (scrollValue / (tinggiDokumen - tinggiJendela)) * 100;
+
+// navbar start
+const nav = $(".container-nav");
+const cNavbar = $(".container-navigation-bar");
+
+$(".show").click(function () {
+  nav.css({
+    transform: "translate(0%)",
+  });
+  cNavbar.css({
+    transform: "translateY(-100%)",
+  });
+  $(".f-container-body").css({
+    filter: "blur(6px)"
+  });
 });
+
+$(".hide").click(function () {
+  nav.css({
+    transform: "translate(-110%)",
+  });
+  cNavbar.css({
+    transform: "translateY(0%)",
+  });
+  $(".f-container-body").css({
+    filter: "blur(0px)"
+  });
+});
+
+// navbar end
+
+// home start
+$(document).ready(function () {
+  $(".container-contain-home").css({
+    transform: "scale(1)",
+  });
+});
+// home end
+
+// gallery start
+const img = $(".slider-img");
+let counter = 1;
+const J = 5;
+$(".left").click(function () {
+  counter = counter - 1;
+  if (counter < 1) {
+    counter = J;
+  }
+  img.attr("src", `./assets/images/${counter}.jpg`);
+});
+
+$(".right").click(function () {
+  counter = counter + 1;
+  if (counter > J) {
+    counter = 1;
+  }
+  img.attr("src", `./assets/images/${counter}.jpg`);
+});
+// gallery end
+
 $(document).scroll(function () {
   const scrollValue = $(this).scrollTop();
-  // console.log(scrollValue);
+  const tinggiDokumen = $(document).height();
+  const tinggiJendela = $(window).height();
 
-  if (scrollValue >= 350) {
-    $("img.s1").css({
-      transform: "scale(1)",
-      transition: "all 1s",
-    });
-    $(".s2").css({
-      transform: "scale(1)",
-      transition: "all 1s",
+  const persScroll = (scrollValue / (tinggiDokumen - tinggiJendela)) * 100;
+  console.log(persScroll);
+  //navbar start
+  if (persScroll >= 1) {
+    $(".container-navigation-bar").css({
+      borderBottom: "1px solid #fff",
     });
   } else {
-    $(".s1").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
-    });
-    $(".s2").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
+    $(".container-navigation-bar").css({
+      borderBottom: "1px solid rgba(255, 255, 255, 0)",
     });
   }
+  //navbar start
 
-  if (scrollValue >= 700) {
-    $(".s3").css({
-      transform: "scale(1)",
-      transition: "all 1s",
-    });
-    $(".s4").css({
-      transform: "scale(1)",
-      transition: "all 1s",
-    });
-  } else {
-    $(".s3").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
-    });
-    $(".s4").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
-    });
-  }
-  if (scrollValue >= 1500) {
-    $(".s5").css({
-      transform: "scale(1)",
-      transition: "all 1s",
-    });
-  } else {
-    $(".s4").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
-    });
-  }
-
-  if (scrollValue >= 2230) {
-    $(".logoExs").css({
-      transform: "scale(1)",
-      transition: "all 1s",
-    });
-  } else {
-    $(".logoExs").css({
-      transform: "scale(.1)",
-      transition: "all 1s",
-    });
-  }
+  // information start
+  // information end
 });
